6c4d0b07ec05adcd6e9014dc298a7a21fed592f5
L0serj3rry <dsyourshy@qq.com>
2023-03-15 21:03:19 +0800

    自动避开不存在的bin目录

--- worker/path.ts
+++ worker/path.ts
@@ -3,7 +3,9 @@ import {EPT_DIR} from "./constants";
 import path from "path";
 
 function getPaths() {
-    return fs.readdirSync(path.join(EPT_DIR,"bin"))
+    const p=path.join(EPT_DIR,"bin")
+    if(!fs.existsSync(p)) return []
+    return fs.readdirSync(p)
 }
 
 export {
