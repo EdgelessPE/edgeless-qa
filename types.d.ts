0f3ee4c63a8bc98416ddf9f7df804583c4ead6b9
Cno <dsyourshy@qq.com>
2025-03-09 23:53:31 +0800

    chore: add biome

--- types.d.ts
+++ types.d.ts
@@ -1,53 +1,53 @@
-import { Result } from "ts-results";
-import { MetaResult } from "./bindings/MetaResult";
+import type { Result } from "ts-results";
+import type { MetaResult } from "./bindings/MetaResult";
 
 export interface Task {
-  scope: string;
-  nepName: string;
-  fileName: string;
-  download: string;
+	scope: string;
+	nepName: string;
+	fileName: string;
+	download: string;
 }
 
 export interface StartRes {
-  task: Task;
-  eptDownload?: string;
+	task: Task;
+	eptDownload?: string;
 }
 
 interface Meta {
-  installed: {
-    at: string;
-    shortcutsAdded: string[];
-    pathsAdded: string[];
-  };
-  uninstalled: {
-    appRemoved: boolean;
-    appData: string[];
-  };
-  nep: MetaResult;
+	installed: {
+		at: string;
+		shortcutsAdded: string[];
+		pathsAdded: string[];
+	};
+	uninstalled: {
+		appRemoved: boolean;
+		appData: string[];
+	};
+	nep: MetaResult;
 }
 
 export interface EndReq {
-  scope: string;
-  nepName: string;
-  fileName: string;
-  result: Result<{ readme: string; meta: Meta }, string>;
+	scope: string;
+	nepName: string;
+	fileName: string;
+	result: Result<{ readme: string; meta: Meta }, string>;
 }
 
 type ShotStage = "afterInstall" | "onRun" | "afterUninstall";
 export interface TakeShotReq {
-  scope: string;
-  nepName: string;
-  fileName: string;
-  stage: ShotStage;
+	scope: string;
+	nepName: string;
+	fileName: string;
+	stage: ShotStage;
 }
 
 export interface TakeShotRes {
-  fileName: string;
+	fileName: string;
 }
 
 export interface StdoutShot {
-  pathName: string;
-  stdout: string;
+	pathName: string;
+	stdout: string;
 }
 
 export type AuxiliaryStage = "install" | "uninstall";
